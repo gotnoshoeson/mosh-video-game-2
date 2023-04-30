19bbd7c8c79c8768dfce7540c23407cca3427ec7
gotnoshoeson <miles_patterson@icloud.com>
2023-04-30 16:22:36 -0700

    Refactored Fetch Games, forgot to commit previous React Query refactors

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "7ff677debee94bcd8c36d5a85cefb813",
   },
 });
